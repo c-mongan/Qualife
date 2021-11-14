ce730fd3fa9906d0827d20792603ad396a1e2305
c-mongan <c17359016@mytudublin.ie>
2021-11-14 17:14:27 +0000

    Updated mainscreen.dart

--- bmi_calculator_app/lib/main_screen.dart
+++ bmi_calculator_app/lib/main_screen.dart
@@ -28,64 +28,59 @@ class _MainScreenState extends State<MainScreen> {
            Expanded(
             child:Row(
               children: <Widget>[
-
-
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Color(0xFFffffff),),
-                   )
+                 Expanded(child: ContainerBox()
                    ),
-
-
-                   Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Color(0xFFffffff),),
-                   )
+                   Expanded(child: ContainerBox(),
                    )
                     ],
                     ) 
           ),
-
-
-         
-          Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Color(0xFFffffff),),
-                   )
+          Expanded(child: ContainerBox()
                    ),         
 Expanded(
             child:Row(
               children: <Widget>[
-
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Color(0xFFffffff),),
-                   )
+              Expanded(child:ContainerBox()
                    ),
-
-
-                   Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Color(0xFFffffff),),
-                   )
+                   Expanded(child: ContainerBox()
                    )
                     ],
-                    ) 
-         
-        
+                    )     
 )
         ]
       ),
     );
+  }
+}
+
+
+//Ctrl + . while hovering over widget lets us extract 
+//the widget and name it , in this case it was named
+//ContainerBox. Its implementation is below.
+
+//This allows us to remove duplicate code
+class ContainerBox extends StatelessWidget {
+  const ContainerBox({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Color(0xFFffffff),
+        boxShadow: 
+          [BoxShadow( color: Colors.grey.withOpacity(0.5),
+          spreadRadius: 5.0,
+          blurRadius: 7.0,
+          offset: Offset(0,3),
+          ),
+          ],
+          
+ ),
+ );
+         
   }
 }
